539d349a73da3e41d24af85fea39dacb1198a028
PostMetalFun1992 <kabanovkirill54@gmail.com>
2019-02-07 14:55:09 +0700

    Basic package configuration

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 ;; Set custom file
 (setq custom-file (expand-file-name "custom.el" emacs-conf-dir))
 (when (file-exists-p custom-file)
-  (load custom-file))
+  (load custtom-file))
 
 ;; No useless bars
 (menu-bar-mode -1)
@@ -20,3 +20,33 @@
 
 ;; Line numbers
 (global-linum-mode t)
+
+;; Package-el config
+(require 'package)
+(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "http://melpa.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+(use-package solarized-theme
+  :ensure t)
+
+(use-package helm
+  :ensure t)
+
+(use-package treemacs
+  :ensure t
+  :config
+  (progn
+    (setq treemacs-show-hidden-files t
+	  treemacs-sorting 'alphabetic-desc))
+  :bind
+  (:map global-map
+        ("C-x t t" . treemacs)))
